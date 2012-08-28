6ace02187a023cad8b01063cb9411da80d742186
qianshen <qianshen1980@gmail.com>
2012-08-28 17:39:20 +0800

    update unit test

---
+++ Test/Test1.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+
+namespace Test
+{
+	[TestFixture()]
+	public class Test1
+	{
+		[Test()]
+		public void TestCase ()
+		{
+			Console.WriteLine("haha");
+		}
+	}
+}
+
